9cc9bcaa808b8ac9ff95aef6ada63f983d8e5c89
Prapra <prapra@gmail.com>
2026-03-17 17:55:20 +0800

    done about

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,16 @@
-import { Geist, Geist_Mono } from "next/font/google"
+import './globals.css';
+import type { Metadata } from 'next';
 
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+export const metadata: Metadata = {
+  title: 'Christian.dev | Software Developer',
+  description: 'A modern personal portfolio built with Next.js and TailwindCSS',
+};
 
-const fontSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
-
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", fontSans.variable)}
-    >
+    <html>
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,12 @@
-import { Button } from "@/components/ui/button"
 
-export default function Page() {
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
+    <div className="container mx-auto px-4">
+      <div className="py-20 border-t border-slate-200 dark:border-white/5">
       </div>
     </div>
-  )
+  );
 }
